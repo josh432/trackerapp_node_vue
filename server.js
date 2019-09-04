11853729ea6dad8b950b091cf38fc0f7756f2238
Josh Silverman <jrsilverman71@hotmail.com>
2019-09-04 10:04:33 -0700

    setting up ENV variables

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 
+// Load env
+dotenv.config({ path: "./config.env" });
+
 const app = express();
 
-app.listen(5000, () => {
-  console.log("App listening on port 5000!");
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
 });
