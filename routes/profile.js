37ff3818d50b9c4a3d8e5d322a9b03e388b36b76
Josh Silverman <jrsilverman71@hotmail.com>
2019-09-04 11:03:36 -0700

    check for error array, backend completed

--- routes/profile.js
+++ routes/profile.js
@@ -18,6 +18,12 @@ router.get("/:platform/:gamertag", async (req, res) => {
     );
 
     const data = await response.json();
+    if (data.errors && data.errors.length > 0) {
+      return res.status(404).json({
+        message: "Profile Not Found"
+      });
+    }
+
     res.json(data);
   } catch (err) {
     console.error(err);
